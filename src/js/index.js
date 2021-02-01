903bceb5e276b50860991b992591c0368e556065
Michal Samulak <sanchossjmistik@gmail.com>
2021-02-01 12:43:30 +0000

    app logic

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,21 @@ import '../scss/main.scss';
 
 /* place your code below */
 
+function removeEffect(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('sound');
+  }
+
+  function musicPlay(e) {
+    const audio = document.querySelector(`audio[data-letter="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-letter="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('sound');
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.letter'));
+  keys.forEach(letter => letter.addEventListener('transitionend', removeEffect));
+  window.addEventListener('keydown', musicPlay);
