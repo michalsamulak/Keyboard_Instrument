72f4c3c8abb515a9561bc1f9f355dfff79e9e3ec
Michal Samulak <sanchossjmistik@gmail.com>
2021-02-02 11:43:06 +0000

    replay and reset functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 import '../scss/main.scss';
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
 
-/* place your code below */
+const btnReplay = document.querySelector('.btn-replay');
+const btnReset = document.querySelector('.btn-reset');
+const keys = Array.from(document.querySelectorAll('.letter'));
+let loop = [];
 
 function removeEffect(e) {
     if (e.propertyName !== 'transform') return;
@@ -14,13 +14,42 @@ function removeEffect(e) {
   function musicPlay(e) {
     const audio = document.querySelector(`audio[data-letter="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-letter="${e.keyCode}"]`);
+
     if (!audio) return;
 
     key.classList.add('sound');
     audio.currentTime = 0;
     audio.play();
+    loop.push(key.dataset.letter);
+  
+    keys.forEach(letter => letter.addEventListener('transitionend', removeEffect));
+
   }
 
-  const keys = Array.from(document.querySelectorAll('.letter'));
-  keys.forEach(letter => letter.addEventListener('transitionend', removeEffect));
+  function loopPlaying(e) {
+    const muzyka = document.querySelector(`audio[data-letter="${e}"]`);
+    const klucz = document.querySelector(`div[data-letter="${e}"]`);
+    klucz.classList.add('sound');
+    muzyka.currentTime = 0;
+    muzyka.play();
+
+
+  }
+
+
+
+
+
   window.addEventListener('keydown', musicPlay);
+
+  const loopPlay = () => {
+    loop.forEach(el => loopPlaying(el));
+
+  }
+
+
+
+  btnReplay.addEventListener('click', loopPlay);
+  btnReset.addEventListener('click', () => {
+    loop = [];
+  })
